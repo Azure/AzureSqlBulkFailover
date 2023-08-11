7bdd0bb70bd137b85650fe60487875ca892c27d4
bartduncan <bartduncan@hotmail.com>
2023-08-11 15:28:39 -0400

    Update AzureSqlBulkFailover.ps1

--- Source/AzureSqlBulkFailover.ps1
+++ Source/AzureSqlBulkFailover.ps1
@@ -1,5 +1,9 @@
-#
-Write-Output "Hello: " 
+Param(
+  [Parameter(Mandatory=$false)]
+  [PSObject]$ScriptProperties
+  )
+
+Write-Output "Hello: $($ScriptProperties.Execute)" 
 Start-Sleep -Seconds 1
 foreach ($i in 1..10) {
     Write-Output "World $i"
