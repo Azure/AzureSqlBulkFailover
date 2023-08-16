598558af9833b3d57c89dbf161d4920ccf614737
Eduardo Rojas <eduardoro@microsoft.com>
2023-08-16 18:52:29 -0400

    added more var logging

--- Source/TestAz.ps1
+++ Source/TestAz.ps1
@@ -4,10 +4,11 @@ Write-Output "Testing: Disable-AzContextAutosave -Scope Process"
 Disable-AzContextAutosave -Scope Process
 Write-Output "Testing: Connect-AzAccount -Identity"
 $AzureContext = (Connect-AzAccount -Identity).context
+$AzureContext
 $subscriptionId = $AzureContext.Subscription
 Write-Output "Testing: Set-AzContext -SubscriptionName"
 Set-AzContext -SubscriptionName $subscriptionId -DefaultProfile $AzureContext
 Write-Output "Testing: Get-AzResourceGroup"
-$out = Get-AzResourceGroup | Select-Object -ExpandProperty resourceGroupName
-Write-Object $out
+Get-AzResourceGroup
+Write-Object "ResourceGroup = $out"
 Write-Output "Testing Complete."
